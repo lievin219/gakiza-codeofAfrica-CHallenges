076d3d81c2758d5b32cb8397c80f28f5152a058c
lievin219 <gakizalievin219@gmail.com>
2024-06-06 00:02:42 +0200

    arraychalennge updated

--- Challenges/arrayMap.js
+++ Challenges/arrayMap.js
@@ -1,20 +1,26 @@
  function arraymapping(arra,target){
-     let sum=0;
-     let currentsum=0;
-      for(let i=0;i<arra.length;i++){
-          currentsum=currentsum+arra[i]
-           while(currentsum>target && sum<=i){
+     let sum=0; //
+     let currentsum=0;  // this is where the summation will be made of all number in an arra(array we will be using)
+      
+     
+     for(let i=0;i<arra.length;i++){ // this is a loop which will pass through each element in an array 
+          currentsum=currentsum+arra[i] // this will make a sum of all elements in an array 
+           
+          while(currentsum>target && sum<=i){ // when the loop reaches where currentsum is greater than target it will start removing an element at the beginning
             currentsum -=arra[sum]
             sum++
            }
-          if(currentsum===target){
-            return true
+      
+          if(currentsum===target){ // this if staement will check if the current sum is equal to the target we entered 
+            return true    // if condition is true it will return true
+              
                }
+
 
       }
      
     
-          return false
+          return false     // otherwise it will return false if it finds that 
    
 
 
